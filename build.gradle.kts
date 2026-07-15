f147a91f4922c20cc85f8062899fa62950efd66a
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-07-15 10:49:03 +0900

    project init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+}
+
+group = "com"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
